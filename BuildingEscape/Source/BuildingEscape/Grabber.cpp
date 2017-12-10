9f9cd11feb0d6d25162a19ef2eeb67aea4fa317a
SourceTreeIsGay <huozuyinshua@gmail.com>
2017-12-11 07:40:31 +0900

    Tutorial74

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,8 @@
 #include "Grabber.h"
 
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -38,11 +40,24 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		PlayerViewPointLocation, PlayerViewPointRotation
 	);
 
-	UE_LOG(LogTemp, Warning, TEXT("Location:%s,Position:%s"),
+	/*UE_LOG(LogTemp, Warning, TEXT("Location:%s,Position:%s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString()
-		);
+		);*/
 	
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector()*Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0,
+		0,
+		10.f
+	);
+
 	// ...
 }
 
